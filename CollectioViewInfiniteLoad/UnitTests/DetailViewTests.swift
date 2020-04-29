67c468d89faf0987ee69d0f7218e4edeb85fab5f
Sowrirajan Sugumaran <ssowri1@gmail.com>
2020-04-29 16:56:03 +0530

    Unit test committed

--- CollectioViewInfiniteLoad/UnitTests/DetailViewTests.swift
+++ CollectioViewInfiniteLoad/UnitTests/DetailViewTests.swift
@@ -21,6 +21,7 @@ class DetailViewTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        galleryData = nil
     }
 
     func testFilter() {
